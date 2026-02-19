78eaa16b2176969d0916f27f42f57bc609b82c4e
Shyam55555 <38212366+Shyam55555@users.noreply.github.com>
2026-02-19 11:52:33 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,19 +4,38 @@ resource "azurerm_resource_group" "poc21_rg" {
 }
 
 resource "azurerm_kubernetes_cluster" "poc21_aks" {
+
   name                = "POC-21-aks"
   location            = azurerm_resource_group.poc21_rg.location
   resource_group_name = azurerm_resource_group.poc21_rg.name
   dns_prefix          = "poc21"
 
+  # ✅ FIX 1: Enable RBAC
+  role_based_access_control_enabled = true
+
   default_node_pool {
-    name       = "poc21node"
-    node_count = 2
-    vm_size    = "Standard_B2s"
+    name            = "poc21node"
+    node_count      = 2
+    vm_size         = "Standard_B2s"
+    os_disk_size_gb = 30
   }
 
   identity {
     type = "SystemAssigned"
+  }
+
+  # ✅ FIX 2: Restrict API Server Access
+  api_server_access_profile {
+    authorized_ip_ranges = [
+      "YOUR_PUBLIC_IP/32"
+    ]
+  }
+
+  # ✅ FIX 3: Enable Network Policy
+  network_profile {
+    network_plugin    = "azure"
+    network_policy    = "azure"
+    load_balancer_sku = "standard"
   }
 
   tags = {
@@ -24,4 +43,3 @@ resource "azurerm_kubernetes_cluster" "poc21_aks" {
     Project     = "POC-21"
   }
 }
-
