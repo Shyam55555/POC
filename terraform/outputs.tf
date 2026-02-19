05f2c59f6ce6dd1b8b3511739d6b74902edef4f1
Shyam55555 <38212366+Shyam55555@users.noreply.github.com>
2026-02-20 00:15:05 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,11 @@
-output "poc21_aks_name" {
+output "aks_name" {
   value = azurerm_kubernetes_cluster.poc21.name
 }
 
-output "poc21_resource_group" {
+output "aks_resource_group" {
   value = data.azurerm_resource_group.devops.name
 }
 
-output "poc21_location" {
+output "aks_location" {
   value = azurerm_kubernetes_cluster.poc21.location
 }
