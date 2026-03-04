10092d15df48e13838634dfdd87e249f7512a497
Shyam55555 <38212366+Shyam55555@users.noreply.github.com>
2026-03-04 16:24:38 +0000

    Create monitoring.tf

---
+++ terraform/monitoring.tf
@@ -0,0 +1,14 @@
+resource "helm_release" "kube_prometheus_stack" {
+  name       = "monitoring"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+
+  namespace        = "monitoring"
+  create_namespace = true
+
+  timeout = 600
+
+  depends_on = [
+    azurerm_kubernetes_cluster.aks
+  ]
+}
